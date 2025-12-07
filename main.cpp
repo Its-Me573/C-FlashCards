f8ef5e55b66f0d64e5ff6d6a4aa1c265763671ee
ItsMe <231827038+Its-Me573@users.noreply.github.com>
2025-12-06 20:12:55 -0800

    read create and write flash card fixed

--- main.cpp
+++ main.cpp
@@ -11,12 +11,12 @@ struct flashCard{
 
 bool isEmpty();//see if the flashCard file is empty
 void readingFlashCard(vector<flashCard> &);//reading the flash card
-void writingToFile(vector<flashCard>, ofstream);//writing flash card to file
 void creatingFlashCard(vector<flashCard> &);//create new flashcard in the vector and add to the file with writingToFile
+void writingToFile(vector<flashCard> &);//writing flash card to file
 void useFlashCards(vector<flashCard>);//use the flash cards with random features
 
 int main(){
-    vector<flashCard> allFlashCards = {};
+    vector<flashCard> allFlashCards;
     string cardQuestion;
     string cardAnswer;
 
@@ -26,17 +26,27 @@ int main(){
     //1. Use Flash cards
     //2. Make new flash card
     //3. Delete flash card
+    /**
     cout << "What would you like to do?" << endl;
     cout << "1. Use flash card" << endl
          <<  "2. Make new flash card" << endl 
          << "3. Delete flash card" << endl;
     cout << "->(1,2,3)";
 
-    int userChoice;
-    cin >> userChoice;
-
-    cout << "testing file reading" << endl;
+    string userChoice;
+    getline(cin, userChoice);
+    int optionSeleceted = stoi(userChoice);
+    **/
     readingFlashCard(allFlashCards);
+    creatingFlashCard(allFlashCards);
+    writingToFile(allFlashCards);
+    
+    
+    for(int i = 0; i < allFlashCards.size(); i++){
+        cout << allFlashCards[i].question << endl;
+        cout << allFlashCards[i].question << endl;
+    }
+    
 }
 
 bool isEmpty(){
@@ -58,16 +68,39 @@ void readingFlashCard(vector<flashCard> &flashCardsStorage){
     ifstream readingFile;
     readingFile.open("flashCard.txt");
     string tempString;
-
+    
     int currentIndex = 0;
     while(getline(readingFile, tempString)){
         if(tempString.substr(0, tempString.find('.')) == "Q"){
             flashCardsStorage.push_back(flashCard());
-            flashCardsStorage[currentIndex].question = tempString;
-            cout << flashCardsStorage[currentIndex].question << endl;
+            flashCardsStorage[currentIndex].question = tempString.substr(tempString.find('.') + 1);
         }else{
-            flashCardsStorage[currentIndex].answer = tempString;
-            cout << flashCardsStorage[currentIndex].answer << endl;
+            flashCardsStorage[currentIndex].answer = tempString.substr(tempString.find('.') + 1);
+            currentIndex++;
         }
+    }
+}
+
+void creatingFlashCard(vector<flashCard> &flashCardsStorage){
+    //ask use for 2 strings of flash cards
+    flashCardsStorage.push_back(flashCard());
+    string flashCardHolder;
+   
+    cout << "Write your question: ";
+    getline(cin, flashCardHolder);
+    flashCardsStorage[flashCardsStorage.size() - 1].question = flashCardHolder;
+
+    cout << "Write the answer: ";
+    getline(cin, flashCardHolder);
+    flashCardsStorage[flashCardsStorage.size() - 1].answer = flashCardHolder;
+}
+
+void writingToFile(vector<flashCard> &flashCardsStorage){
+    ofstream writingFile;
+    writingFile.open("flashCard.txt");
+
+    for(int i = 0; i < flashCardsStorage.size(); i++){
+        writingFile << "Q." << flashCardsStorage[i].question << endl;
+        writingFile << "A." << flashCardsStorage[i].answer << endl;
     }
 }
